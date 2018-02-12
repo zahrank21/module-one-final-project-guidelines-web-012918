7a1b1363ac10c764029cc23b569b4c3edf7d88ab
Zahran Kabir <zahran@Zahrans-MacBook-Pro.local>
2018-02-12 16:25:15 -0500

    added initialize, all, findjobswithcompany methods

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,21 @@
 class Company
 
+  attr_accessor :name, :location
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def find_jobs_with_company
+    Job.all.select do |job|
+      job.company == self
+    end
+  end
 end
