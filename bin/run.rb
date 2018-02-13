54365ab36d40d876bd8f0a24a71b582f9c19b05f
Zahran Kabir <zahran@Zahrans-MacBook-Pro.local>
2018-02-13 11:13:21 -0500

    added run.rb requires for migrations

--- bin/run.rb
+++ bin/run.rb
@@ -3,6 +3,11 @@ require_relative '../app/models/adapter.rb'
 require_relative '../app/models/company.rb'
 require_relative '../app/models/job.rb'
 require_relative '../app/models/location.rb'
+require_relative '../db/migrate/001_create_jobs_table.rb'
+require_relative '../db/migrate/002_create_locations_table.rb'
+require_relative '../db/migrate/003_create_companies_table.rb'
+require_relative '../db/migrate/004_insert_name_column_into_jobs.rb'
+
 
 Adapter.api_call
 
