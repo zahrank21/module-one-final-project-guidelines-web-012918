141d9328a6b06afc5e240c19e8d4b1afa74551d5
github username <github email address>
2018-02-13 10:43:17 -0500

    creating db from API for location

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,5 +2,10 @@ class Location < ActiveRecord::Base
   has_many :jobs
   has_many :companies, through: :jobs
 
-  
+  def self.create_table
+    Adapter.api_call.each do |job_hash|
+      Location.create(location: job_hash["location"])
+    end
+  end
+
 end
