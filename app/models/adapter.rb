33e84a5f3a80a462eae4ee6d25b80358bde3b530
github username <github email address>
2018-02-13 09:52:39 -0500

    complete jobs list by looping

--- app/models/adapter.rb
+++ app/models/adapter.rb
@@ -1,11 +1,23 @@
 class Adapter
-
   def self.api_call
-    api = RestClient.get('https://jobs.github.com/positions.json')
+    #grab array of hash info from one page
+    page = 0
+    url = "https://jobs.github.com/positions.json?page=#{page}"
+    # job_array = grab_from_one_page(url)
+    api = RestClient.get(url)
     job_array = JSON.parse(api)
-    binding.pry
+    #grab from others and then concat arrays
+    extra_job_array = [1,1]
+    while extra_job_array.any?
+      page += 1
+      url = "https://jobs.github.com/positions.json?page=#{page}"
+      #extra_job_array = grab_from_one_page(url)
+      api = RestClient.get(url)
+      extra_job_array = JSON.parse(api)
+      job_array.concat(extra_job_array)
+    end
+    job_array
   end
-
 
 
   # def self.get_title(param)
